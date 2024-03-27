e2d87f493d3617cd0346176f6225e18bc1de6b69
ARBUCHELI <radio_seattle@hotmail.com>
2024-03-27 09:46:47 +0300

    #implementation-details - setting 12 initial variables - creating the setPlayerMoves function, and setting the correct global move variables

--- js/game-logic.js
+++ js/game-logic.js
@@ -1,1 +1,40 @@
 // All code should be written in this file.
+/*
+Twelve global variables representing each player’s move types and values (3 move types and 3 move values for each player). 
+These variable names should be in the form of playerOneMoveOneType, playerOneMoveOneValue, etc.
+*/
+
+let playerOneMoveOneType;
+let playerOneMoveTwoType;
+let playerOneMoveThreeType;
+
+let playerTwoMoveOneType;
+let playerTwoMoveTwoType;
+let playerTwoMoveThreeType;
+
+let playerOneMoveOneValue;
+let playerOneMoveTwoValue;
+let playerOneMoveThreeValue;
+
+let playerTwoMoveOneValue;
+let playerTwoMoveTwoValue;
+let playerTwoMoveThreeValue;
+
+function setPlayerMoves(player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) {
+    if (player === 'Player One') {
+        playerOneMoveOneType = moveOneType;
+        playerOneMoveOneValue = moveOneValue;
+        playerOneMoveTwoType = moveTwoType;
+        playerOneMoveTwoValue = moveTwoValue;
+        playerOneMoveThreeType = moveThreeType;
+        playerOneMoveThreeValue = moveThreeValue;
+      } else if (player === 'Player Two') {
+        playerTwoMoveOneType = moveOneType;
+        playerTwoMoveOneValue = moveOneValue;
+        playerTwoMoveTwoType = moveTwoType;
+        playerTwoMoveTwoValue = moveTwoValue;
+        playerTwoMoveThreeType = moveThreeType;
+        playerTwoMoveThreeValue = moveThreeValue;
+      }
+}
+
